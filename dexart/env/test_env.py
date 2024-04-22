9a4b516787c133a152d1f26596eda72c4c2bd607
jiawei <jiaweizhang927@gmail.com>
2024-04-21 20:51:51 -0500

    update arm init pose

--- dexart/env/test_env.py
+++ dexart/env/test_env.py
@@ -12,11 +12,13 @@ if __name__ == '__main__':
     # viewer = env.create_viewer()
     env.seed(0)
     env.simple_reset()
+    
+    env.robot.set_qpos([-1.57, 0 ,0 ,0 ,0 ,0, 0 ] + [1.57, 0 ,0 ,0 ,0 ,0, 0 ] + [0] * 16)
 
     while True:
         for i in range(10000):
             if i % 100 == 0:
-                arm_action = np.random.random(14) * 2 - 1
+                arm_action = np.random.random(12) * 2 - 1
                 finger_action = (np.random.random(16) * 2 - 1) * np.pi
                 action = np.concatenate((arm_action, finger_action), axis=0)
             env.atlas_sim_step(action)
